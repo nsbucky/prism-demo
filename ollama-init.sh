2ab2960a339c562d1486217b39bc482a0589f742
thebusypixel <nsbucky@gmail.com>
2025-04-27 11:21:27 -0700

    docker updated with postgres and ollama init scripts to pull my models

---
+++ ollama-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+/bin/ollama serve &
+
+# Record Process ID
+pid=$!
+
+# Pause for Ollama to start
+sleep 5
+
+echo "loading llama3.2"
+
+ollama pull llama3.2
+# ollama pull mistral
+# ollama pull mwiewior/bielik
+# ollama pull deepseek-r1:7b
+# ollama pull deepseek-coder-v2
+# For embedding capabilities, always pull the mxbai model
+# ollama pull mxbai-embed-large
+
+wait $pid
+
