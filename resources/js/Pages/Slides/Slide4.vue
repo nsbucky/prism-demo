10945060741184e910cc275d95cbba3c0f570b1d
thebusypixel <nsbucky@gmail.com>
2025-05-06 11:57:26 -0700

    wip

--- resources/js/Pages/Slides/Slide4.vue
+++ resources/js/Pages/Slides/Slide4.vue
@@ -11,16 +11,20 @@ import BaseSlide from "../Components/BaseSlide.vue";
                 <div class="text-center">
                     <img crossorigin="anonymous"
                          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/800px-Laravel.svg.png"
-                         class="bg-white rounded-lg shadow-lg p-4 mb-2" height="500" alt="Laravel Logo" />
+                         class="bg-white rounded-lg shadow-lg p-4 mb-2" height="500" alt="Laravel Logo"/>
                     <a href="https://laravel.com" class="text-red">laravel.com</a>
                 </div>
 
                 <div class="ml-4 space-y-4">
-                    <p>Laravel is a robust, progressive, PHP framework renowned for its elegant syntax, developer-friendly tools, and
-                        extensive ecosystem. It excels in making web development faster and more enjoyable for PHP developers.
-                        It is currently on version 12 and supports PHP 8.2 and above.</p>
+                    <p>Laravel is a robust, progressive, PHP framework renowned for its elegant syntax,
+                        developer-friendly tools, and
+                        extensive ecosystem. It excels in making web development faster and more enjoyable for PHP
+                        developers.</p>
 
-                    <p>The framework provides a rich set of features like routing, templating, database migrations, and security
+                    <p>It is currently on version 12 and supports PHP 8.2 and above.</p>
+
+                    <p>The framework provides a rich set of features like routing, templating, database migrations, and
+                        security
                         tools out-of-the-box, streamlining common tasks and fostering rapid application development. Its
                         strong community support and comprehensive documentation further empower PHP developers to build
                         sophisticated and scalable web applications efficiently.</p>
