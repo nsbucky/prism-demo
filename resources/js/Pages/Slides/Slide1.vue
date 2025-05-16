beacaeac108bbef33c484e926e783758651e3e0b
thebusypixel <nsbucky@gmail.com>
2025-05-15 17:00:26 -0700

    homepage update

--- resources/js/Pages/Slides/Slide1.vue
+++ resources/js/Pages/Slides/Slide1.vue
@@ -4,6 +4,7 @@ import llamaImage from '../../assets/llama-al.png'
 import SlideTitle from "../Components/SlideTitle.vue";
 import SlideBody from "../Components/SlideBody.vue";
 import Navigation from "../Components/Navigation.vue";
+import Qr from '@/assets/bitly-qr.png'
 
 </script>
 <template>
@@ -16,18 +17,22 @@ import Navigation from "../Components/Navigation.vue";
                  class="w-1/2 object-contain border-2 border-white h-auto object-contain mx-auto"/>
         </div>
 
-        <div class="mt-8 text-center">
-            <SlideTitle>PHP Tek 2025</SlideTitle>
+        <div class="mt-8">
+            <SlideTitle class="text-center">PHP Tek 2025</SlideTitle>
 
             <SlideBody>
-                <p class="mb-4">Thanks for coming! If you want to follow along you can clone this repo: </p>
-                <code class="bg-gray-800/20 p-4">https://github.com/nsbucky/prism-demo</code>
-                <p class="text-xs text-gray-500 p-4">
-                    Llama in part by <a
-                    href="https://unsplash.com/@lipnorth?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Greg
-                    Lippert</a> on <a
-                    href="https://unsplash.com/photos/white-camel-Z5Yha3jIHnc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-                </p>
+                <div class="flex justify-center mb-6 gap-4">
+                    <div>
+                        <img :src="Qr" alt="Github Repo" class="bg-white rounded-sm shadow-lg p-1 inline-block"
+                             width="200"/>
+                    </div>
+                    <div>
+
+                        <p class="mb-4">Thanks for coming! If you want to follow along you can clone this repo: </p>
+                        <code class="bg-gray-800/20 p-4">https://github.com/nsbucky/prism-demo</code>
+
+                    </div>
+                </div>
             </SlideBody>
 
         </div>
